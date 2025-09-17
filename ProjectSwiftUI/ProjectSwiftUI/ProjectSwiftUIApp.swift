0f5af0f2e9c02165982abffeaea40da39e34f0b6
Leonardo Cunha <cunhacardosoleo@gmail.com>
2025-09-17 14:07:13 -0300

    🏗️: Xcode project basic config

---
+++ ProjectSwiftUI/ProjectSwiftUI/ProjectSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectSwiftUIApp.swift
+//  ProjectSwiftUI
+//
+//  Created by Leonardo Cunha on 17/09/25.
+//
+
+import SwiftUI
+
+@main
+struct ProjectSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
