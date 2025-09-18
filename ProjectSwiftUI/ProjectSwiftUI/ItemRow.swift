8d918892f6fa92f3fd5f1918023a02f9783f1725
Leonardo Cunha <cunhacardosoleo@gmail.com>
2025-09-18 08:50:47 -0300

    ✅: Displaying the restrictions on far right for every menu item

--- ProjectSwiftUI/ProjectSwiftUI/ItemRow.swift
+++ ProjectSwiftUI/ProjectSwiftUI/ItemRow.swift
@@ -9,13 +9,29 @@ import SwiftUI
 
 struct ItemRow: View {
     let item: MenuItem
+    let colors: [String: Color] = ["D": .purple, "G": .black, "N": .red, "S": .blue, "V": .green]
     
     var body: some View {
         HStack {
             Image(item.thumbnailImage)
-            VStack(alignment: .leading){
+                .clipShape(Circle())
+                .overlay(Circle().stroke(.gray, lineWidth: 2))
+            VStack(alignment: .leading) {
                 Text(item.name)
+                    .font(.headline)
                 Text("R$ \(item.price)")
+                }
+            
+            Spacer()
+            
+            ForEach(item.restrictions, id: \.self) { restriction in
+                Text(restriction)
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(5)
+                    .background(colors[restriction, default: .black])
+                    .clipShape(Circle())
+                    .foregroundColor(.white)
             }
         }
     }
